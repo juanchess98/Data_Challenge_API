e653ce8c1ea5574cdb1849b987b0e8241cf8670f
Juan Diego Echavarria <juanchess98@gmail.com>
2024-02-21 19:00:14 -0500

    Define config for database

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration file for the Flask Application and database
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
